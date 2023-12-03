78da9790ea90270c734981a087e924df044d352c
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-12-03 21:25:29 +0530

    final one.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const express= require("express")
 const connectDB=require("./routes/connection")
 const urlroute=require("./routes/routesUrl")
 const URL=require("./models/urlmodel")
+const mongoDB= process.env.Mongo_Url
 const app= express();
 const Port = 3000;
 app.use(express.json())
 
 //creating connection to DB
-connectDB("mongodb+srv://ghimiresandesh45:LJTELiHREqh162rE@c1.r1vog89.mongodb.net/?retryWrites=true&w=majority")
+connectDB(mongoDB)
 .then(()=>
 console.log('mongodb connected')
 )
